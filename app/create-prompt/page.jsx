0ab815adddb84ea479a65f5c543f2f1138b8616d
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-12-22 10:44:09 +0530

    home

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,14 +6,34 @@ import Form from '@components/Form'
 import React from 'react'
 
 const CreatePrompt = () => {
+  const router = useRouter()
+  const {data: session} = useSession()
 const[submitting, setSubmitting]= useState(false)
 const [post, setPost] = useState({
-    promt: "",
+    prompt: "",
     tag: "",
 })
 
 const CreatePrompt = async (e)=>{
-
+e.preventDefault();
+setSubmitting(true);
+try {
+  const response = await fetch('/api/prompt/new',{
+    method: "POST",
+    body: JSON.stringify({
+      prompt: post.prompt,
+      tag: post.tag,
+      userId: session?.user?.id
+    })
+  })
+  if(response.ok){
+    router.push('/')
+  }
+} catch (error) {
+console.log(error);
+} finally{
+  setSubmitting(false)
+}
 }
 
   return (
